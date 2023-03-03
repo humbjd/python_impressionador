1198fb3afdcc425c771964eb33868723b0bee205
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-03-03 11:35:43 -0300

    Criacao dos principios basicos e exercicios

---
+++ aula-01/main.py
@@ -0,0 +1,37 @@
+# Utilização do Print
+
+print('text')
+print(123)
+print(1+1)
+print(2.2 * 5)
+print('Olá Mundo')
+
+print(7 % 3)
+
+print(1 + 2 * 2)
+print((1 + 2 ) * 2)
+
+print('Meu nome é Humberto')
+print(('Seu nome é ' in 'Humberto'))
+print('h' in 'Humberto')
+print('H' in 'Humberto')
+
+
+# Variáveis
+
+qtde_vendas = 1800
+print(qtde_vendas)
+
+faturamento = 150
+custo = 60
+
+lucro = faturamento - custo
+print(lucro)
+
+nome = input('Qual é seu nome')
+print(nome)
+
+
+nome = input('Qual é seu nome ')
+sobrenome = input('Qual seu sobrenome ')
+print(nome + sobrenome)
--- main.py
+++
@@ -1,5 +0,0 @@
-print('text')
-
-print(123)
-
-print(1+1)
