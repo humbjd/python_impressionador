cf201501fb6cadb38455e0f8b85f176b48cfda2a
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-04-20 09:34:13 -0300

    For atualização

---
+++ For/aula-05.py
@@ -0,0 +1,24 @@
+produtos = ['iphone', 'ipad', 'airpod', 'macbook']
+precos = [7000, 10000, 2500, 14000]
+
+# for item in lista
+for preco in precos:
+    print(preco)
+
+# preco com imposto
+
+for preco in precos:
+    print(preco * 1.1)
+
+# for i in range
+for i in range(len(precos)):
+    produto = produtos[i]
+    preco = precos[i]
+    print(produto, preco)
+
+# for item in lista com enumerate
+
+
+
+# preco de cada produto com imposto
+
